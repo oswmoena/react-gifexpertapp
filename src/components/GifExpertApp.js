a2c113abe48485854e433bccd3a473f0271e7411
Oswall Moena <oswmoena@gmail.com>
2020-11-19 00:17:30 -0300

    proyecto completo

---
+++ src/components/GifExpertApp.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { AddCategory } from './AddCategory'
+import { GifGrid } from './GifGrid'
+
+const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch'])
+
+    return (
+        <div>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+            <hr />
+            <ol>
+                {
+                    categories.map((category) => 
+                        <GifGrid
+                            key={category}
+                            category={category}
+                        />
+                    )
+                }
+            </ol>
+        </div>
+    )
+}
+
+export default GifExpertApp
